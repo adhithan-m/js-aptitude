fbc4e8ef515b5fea1f9aec9d49638a5ef074b03a
adharv-adhi78 <adhisprinter@gamil.com>
2022-11-30 09:55:12 +0530

    edit js.

---
+++ js-29/js/45.js
@@ -0,0 +1,7 @@
+var l = parceInt(prompt("Enter the length"));
+var b = parceInt(prompt("Enter the breadth"));
+var h = parceInt(prompt("Enter the height"));
+var s = parceInt(prompt("Enter the space occupied"));
+var vol = l*b*h;
+var p = vol/s;
+alert(p);
